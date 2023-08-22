95ddff4c4a6388b4ea76b0a9ba3051b25510795f
Amund Grorud <amund@Amunds-MBP.home>
2023-08-22 19:50:44 +0200

    gaaawd...

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -20,8 +20,6 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     reg [ BUFFER_SIZE - 1 : 0 ] channels [ 0 : NUMBER_OF_CHANNELS-1 ];
 
-    reg 
-
     reg [(8*10-1):0] test_reg; // 10 8-bit registers
 
     // use bidirectionals as outputs
