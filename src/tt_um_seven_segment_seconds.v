ff6b517fc38d16a981ede70328d24b06b487f3ff
Amund Grorud <amund@Amunds-MBP.home>
2023-08-22 19:48:47 +0200

    Last try. All i well?

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -20,30 +20,15 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     reg [ BUFFER_SIZE - 1 : 0 ] channels [ 0 : NUMBER_OF_CHANNELS-1 ];
 
+    reg 
 
     reg [(8*10-1):0] test_reg; // 10 8-bit registers
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
     assign uio_out = 8'b00000000;
-    /*
-    wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
 
 
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = second_counter[7:0];
-
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
-
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
-
-    */
     assign uo_out = test_reg[7:0];
     always @(posedge clk) begin
         // if reset, set counter to 0
@@ -51,32 +36,14 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
             test_reg <= 0;
             //digit <= 0;
         end else begin
-            for( i = 0; i < NUMBER_OF_CHANNELS; i = i +1 ) begin
-                channels[i] <= channels[i] >> NUMBER_OF_BITS;
-                channels[i][BUFFER_SIZE-1:BUFFER_SIZE-NUMBER_OF_BITS] <= ui_in;
+            for( genvar i = 0; i < NUMBER_OF_CHANNELS; i = i + 1 ) begin
+                reg [BUFFER_SIZE-1:0] updated_value;
+                updated_value = channels[i] >> NUMBER_OF_BITS;
+                updated_value[BUFFER_SIZE-1:BUFFER_SIZE-NUMBER_OF_BITS] = ui_in;
+                channels[i] <= updated_value;
             end
 
-            /*
-            // if up to 16e6
-            if (second_counter == compare) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-            */
         end
     end
-
-    // instantiate segment display
-    // seg7 seg7(.counter(digit), .segments(led_out));
 
 endmodule
