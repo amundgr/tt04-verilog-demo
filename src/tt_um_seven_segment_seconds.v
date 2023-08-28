0ce1a75aa10c6423c5c592d279414daadff578c1
Amund Grorud <amund@Amunds-MBP.home>
2023-08-28 18:08:52 +0200

    semi-fucking-colon

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -32,7 +32,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
             //digit <= 0;
         end else begin
             for( int i = 0; i < NUMBER_OF_CHANNELS; i = i + 1 ) begin
-                channels[i] <= {channels[i][BUFFER_SIZE-2:0], uio_in[i]}
+                channels[i] <= {channels[i][BUFFER_SIZE-2:0], uio_in[i]};
                 //channels[i][:] = channels[i] >> NUMBER_OF_BITS;
                 //channels[i][BUFFER_SIZE-1:BUFFER_SIZE-NUMBER_OF_BITS] = ui_in;
             end
