8fdeadf84ff66b2e9b827e6b0a8d90321f2cd69c
Amund Grorud <amund@Amunds-MBP.home>
2023-08-28 17:53:25 +0200

    fix error

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -23,6 +23,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
     assign uio_out = 8'b00000000;
+    assign uo_out = 8'b00000000;
 
     integer i;
     always @(posedge clk) begin
