c48a9d092ada3c8295d32c14ac150872099c4921
Amund Grorud <amund@grorud.net>
2023-09-02 14:38:43 +0200

    needs the start with tt_um for some reason?

--- src/tt_um_beamformer.v
+++ src/tt_um_beamformer.v
@@ -93,7 +93,7 @@ module channel_buffer (clk, data_in, read_index, data_out);
 
 endmodule
 
-module tt_amundgr_beamformer (
+module tt_um_beamformer (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
