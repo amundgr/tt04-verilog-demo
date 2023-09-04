dd12ffc82bbd5fb4e86f57682fcf7701e3676b83
Amund Grorud <amund@Amunds-MBP.home>
2023-09-04 17:45:36 +0200

    4 channels

--- src/parameters.v
+++ src/parameters.v
@@ -1,7 +1,7 @@
 `ifndef PARAMETERS_V
 `define PARAMETERS_V
 
-parameter NUMBER_OF_CHANNELS = 3; // Channles = channels * 2
+parameter NUMBER_OF_CHANNELS = 4; // Channles = channels * 2
 parameter NUMBER_OF_BITS = 8;
 parameter BUFFER_SIZE = 8;
 
