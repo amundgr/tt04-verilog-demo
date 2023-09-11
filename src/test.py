6dc2aa542349c3b19d38cde163cc41d3a3e1b354
Amund Grorud <amund@grorud.net>
2023-09-11 17:14:56 +0200

    stuff

--- src/test.py
+++ src/test.py
@@ -33,7 +33,9 @@ async def test_beamformer(dut):
 
     res = 0
     for i in range(8):
-        res += (dut.uo_out.value & 1) << i
+        print(type(dut.uo_out.value))
+        tmp = dut.uo_out.value
+        res += (tmp & 1) << i
         await ClockCycles(dut.clk, 1)
 
     dut._log.info(f"Result: {res}")
